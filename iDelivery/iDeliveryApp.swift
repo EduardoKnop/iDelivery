d9b75062c86859613a2c3fb6c4f4dcdfe520f682
Eduardo Knop <eduk.coelho@hotmail.com>
2025-08-25 00:52:10 -0300

    Initial Commit

---
+++ iDelivery/iDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDeliveryApp.swift
+//  iDelivery
+//
+//  Created by Eduardo Knop on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct iDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iDeliveryTests/iDeliveryTests.swift
@@ -0,0 +1,17 @@
+//
+//  iDeliveryTests.swift
+//  iDeliveryTests
+//
+//  Created by Eduardo Knop on 25/08/25.
+//
+
+import Testing
+@testable import iDelivery
+
+struct iDeliveryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
