64ec9a7408adcb31a204fecafd5d872f514d41d3
Eduardo Knop <eduk.coelho@hotmail.com>
2025-08-30 11:00:57 -0300

    Create Product Detail Screen

---
+++ iDelivery/ProductView/AddCartButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddCartButton.swift
+//  iDelivery
+//
+//  Created by Eduardo Knop on 30/08/25.
+//
+
+import SwiftUI
+
+struct AddCartButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCartButton()
+}
